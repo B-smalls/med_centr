1ade7ae1546e3a6b23e42f105be21e36d2ea4353
b-smalls <kuzn.dev@gmail.com>
2023-11-12 18:13:21 +0300

    Добавление Joser и DRF

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from api.spectacular.urls import urlpatterns as doc_urls
-
+from django.urls import path, include
 app_name = 'api'
 
-urlpatterns = []
+urlpatterns = [
+    path('auth/', include('djoser.urls.jwt'))
+]
 
 urlpatterns += doc_urls
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import environ
 
 root = environ.Path(__file__) - 2
@@ -25,7 +27,8 @@ INSTALLED_APPS = [
 # package
 INSTALLED_APPS += [
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'djoser'
 ]
 
 # apps
@@ -155,7 +158,8 @@ SPECTACULAR_SETTINGS = {
     ],
 
     'SERVE_AUTHENTICATION': [
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
 
     'SWAGGER_UI_SETTINGS': {
@@ -165,4 +169,41 @@ SPECTACULAR_SETTINGS = {
 
     'COMPONENT_SPLIT_REQUEST': True,
     'SORT_OPERATIONS': False,
+}
+
+######################
+# JOSER
+######################
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': False,
+    'SERIALIZERS': {},
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FILED': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken', ),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFREASH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=1),
+    'SLIDING_TOKEN_REFREASH_LIFETIME': timedelta(days=7),
 }
