fda1f4bdc86eaf65d71fdc5496a6c02ff0e82708
b-smalls <kuzn.dev@gmail.com>
2023-11-17 20:18:24 +0300

    fixed

--- clients/admin.py
+++ clients/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from clients.models.clients import Client
 
 # Register your models here.
 
-@admin.register(User)
+@admin.register(Client)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'first_name', 'last_name')
 
