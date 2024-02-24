76584ed47751066116e00c3ded4d12cc79ce114b
b-smalls <kuzn.dev@gmail.com>
2024-02-24 21:33:43 +0300

    Доделан микросервис "Мед. услуги".

--- medservices/serializers/api/servRecord.py
+++ medservices/serializers/api/servRecord.py
@@ -4,42 +4,64 @@ from rest_framework import serializers
 from medservices.models.servShedule import ServShedule
 from medservices.models.servRecord import ServRecord
 from django.db import transaction
+from medservices.serializers.nested import services
 from rest_framework.exceptions import ParseError
 from django.http import HttpResponse
 
-class ServRecordSerializer(serializers.ModelSerializer):
+class ServRecordCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServRecord
-
         fields = (
             'id',
             'servRecord_date',
-            'account_id',
             'sshed_id',
         )
 
-    def create(self):
-        user = self.context['request'].user
-        sshed_id = self.context.get('sshed_id')
-        current_date = timezone.now().date()
+    def create(self, validated_data):
+        try:
+            sshed_id = self.initial_data.get('sshed_id')
+            account_id = self.context['request'].user.id  # Получаем id пользователя из запроса
 
+            with transaction.atomic():
+                record = ServRecord.objects.create(account_id=account_id, **validated_data)
+
+                ServShedule.objects.filter(id=sshed_id).update(serv_status=False)
+        except Exception as e:
+            raise ParseError(f"Ошибка при создании записи: {e}")
+
+        return record
+class ServRecordViewSerializer(serializers.ModelSerializer):
+    serv_name = serializers.CharField(source='sshed_id.serv_id.serv_name', read_only=True)
+    serv_day = serializers.DateField(source='sshed_id.serv_day', read_only=True)
+    serv_hours = serializers.TimeField(source='sshed_id.serv_hours', read_only=True)
+
+    class Meta:
+        model = ServRecord
+        fields = (
+            'id',
+            'servRecord_date',
+            'serv_name',
+            'serv_day',
+            'serv_hours',
+        )
+
+
+class ServRecordDeleteSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+    def delete_record(self):
+        serv_record_id = self.validated_data.get('id')
         try:
             with transaction.atomic():
-                # Создаем запись с указанием текущей даты, ID пользователя и sshed_id
-                serv_record = ServRecord.objects.create(
-                    servRecord_date=current_date,
-                    account_id=user.id,
-                    sshed_id=sshed_id,
-                )
+                serv_record = ServRecord.objects.select_for_update().get(id=serv_record_id)
+                if serv_record.account_id != self.context['request'].user.id:
+                    raise serializers.ValidationError("У вас нет прав на удаление этой записи")
 
-                # Обновляем 'serv_status' в модели ServShedule
-                schedule_instance = ServShedule.objects.get(id=sshed_id)
-                schedule_instance.serv_status = False
-                schedule_instance.save()
+                serv_record.delete()  # Удаление записи ServRecord
 
-        except ServShedule.DoesNotExist:
-            raise serializers.ValidationError(f"servShedule с sshed_id {sshed_id} не найден.")
-        except Exception as e:
-            raise serializers.ValidationError(f"Произошла ошибка при создании записи: {str(e)}")
-
-        return serv_record
+                # Обновление serv_status в связанной записи ServShedule
+                serv_shedule = serv_record.sshed_id
+                serv_shedule.serv_status = True
+                serv_shedule.save()
+        except ServRecord.DoesNotExist:
+            raise serializers.ValidationError("Запись не найдена")
--- medservices/urls.py
+++ medservices/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('medserv/serv/servshed-list/', servShedule.ServSheduleView.as_view(), name='servshed-list'),
     path('medserv/serv/search-servshed/<int:serv_id>/', servShedule.SearchSheduleView.as_view(), name='search-servshed'),
     path('medserv/serv/create-serv-record/', servRecord.ServRecordCreateView.as_view(), name='create-serv-record'),
+    path('medserv/serv/delete-serv-record/<int:pk>/', servRecord.ServRecordDeleteView.as_view(), name='delete-serv-record'),
+    path('medserv/serv/user-serv-record/', servRecord.ServRecordListView.as_view(), name='user-serv-record'),
 
    # path('clients/change-passwd/', clients.ChangePasswordView.as_view(), name='change_passwd'),
 ]
