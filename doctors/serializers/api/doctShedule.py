2d3fb21519caef6f19fcad98c92b8e8545bcfb08
b-smalls <kuzn.dev@gmail.com>
2024-02-26 16:23:29 +0300

    Начало разработки микросевриса "Doctor". Созданы директории и первоначальные сериализаторы.

---
+++ doctors/serializers/api/doctShedule.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from doctors.models.doctShedule import DoctShedule
+from doctors.serializers.api.doctor import DoctorSerializer
+
+class DoctorSheduleSerializer(serializers.ModelSerializer):
+
+    doctor_id = DoctorSerializer()
+    class Meta:
+        model = DoctShedule
+        fields = (
+            'id',
+            'doc_day',
+            'doct_hours',
+            'doct_status',
+            'doct_status',
+            'doctor_id'
+        )
